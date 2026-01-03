f2d41ae1162b3b70b75490cc9147ebea3c407b1c
Peter0x2 <peter0x2@outlook.com>
2026-01-03 23:19:16 +0800

    use detail from booknode for post settlement

--- src/utils/priceUtil.ts
+++ src/utils/priceUtil.ts
@@ -12,17 +12,3 @@ function _calculateDeviationBps(actual: bigint, expected: bigint) {
     const diff = actual > expected ? actual - expected : expected - actual;
     return (diff * 10000n) / expected;
 }
-
-//write a main method to test the checkPrice function
-function main() {
-    const amountBase = BigInt(50000000);
-    const amountQuote = BigInt(49977509);
-    const decimalBase = 6;
-    const decimalQuote = 6;
-    const price = 1.00045002;
-    const toleranceBps = 10n;
-    const result = checkPrice(amountBase, amountQuote, decimalBase, decimalQuote, price, toleranceBps);
-    console.log(result);
-}
-
-main();
